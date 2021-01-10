aaf46e22c737c2a61eb4b8cc697d0829aa8096e8
hwayoung98 <hwa3270@gmail.com>
2021-01-10 21:27:40 +0900

    [2021-01-10] sum solve complete

---
+++ 1_기본수학/2_sum.cpp
@@ -0,0 +1,13 @@
+// title: [8393] 합
+// link: https://www.acmicpc.net/problem/8393
+
+#include <iostream>
+
+using namespace std;
+
+int main(void) {
+    int n, sum;
+    cin >> n;
+    sum = n*(n+1)/2;
+    cout << sum << endl;
+}
