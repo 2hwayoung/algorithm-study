cf185e673c39e4451aab234b68255f2e106c877c
hwayoung98 <hwa3270@gmail.com>
2021-01-10 19:27:03 +0900

    [2020-01-10] sort_k번째수 solved complete

---
+++ sort_k번째수.cpp
@@ -0,0 +1,22 @@
+// source_link: https://programmers.co.kr/learn/courses/30/lessons/42748
+// score = 1000+2
+
+#include <string>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int slice(vector<int> &array, vector<int> &sub_command) {
+    vector<int> sub_array(sub_command[1]-sub_command[0]+1);
+    copy(array.begin() + sub_command[0] - 1, array.begin() + sub_command[1], sub_array.begin());
+    sort(sub_array.begin(), sub_array.end());
+    return sub_array[sub_command[2]-1];
+}
+
+vector<int> solution(vector<int> array, vector<vector<int>> commands) {
+    vector<int> answer;
+    for (int i=0; i < commands.size(); i++)
+        answer.push_back(slice(array, commands[i]));
+    return answer;
+}
