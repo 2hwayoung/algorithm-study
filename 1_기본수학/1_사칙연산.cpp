2e5e27c7a8d32b4cc931dc46a837b98281726c73
hwayoung98 <hwa3270@gmail.com>
2021-01-10 21:27:25 +0900

    [2021-01-10] 사칙연산 solve complete

---
+++ 1_기본수학/1_사칙연산.cpp
@@ -0,0 +1,17 @@
+// title: [10869] 사칙연산
+// link: https://www.acmicpc.net/problem/10869
+
+#include <iostream>
+
+using namespace std;
+
+int main(void)
+{
+    int a, b;
+    cin >> a >> b;
+    cout << a+b << endl;
+    cout << a-b << endl;
+    cout << a*b << endl;
+    cout << a/b << endl;
+    cout << a%b << endl;
+}
