8f380ed61f135fb069c18eee047f30c0b8f9c8c9
2hwayoung <hwa3270@gmail.com>
2021-02-14 22:58:54 +0900

    [2021-02-14] 주식가격 solve complete

---
+++ 자료구조/stack_스택/주식가격.cpp
@@ -0,0 +1,24 @@
+// title: 프로그래머스>주식가격
+// link: https://programmers.co.kr/learn/courses/30/lessons/42584
+// 분류: stack/queue
+
+#include <string>
+#include <vector>
+#include <stack>
+
+using namespace std;
+
+vector<int> solution(vector<int> prices) {
+    vector<int> answer;
+    for (int i = 0; i < prices.size(); i++) {
+        int period = 0;
+        for (int j=i+1; j < prices.size(); j++) {
+            if (prices.at(j) < prices.at(i)) {
+                period++;
+                break;
+            } else {period++;}
+        }
+        answer.push_back(period);
+    }
+    return answer;
+}
