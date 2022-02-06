24b428ed3a8fb49323411b1cfb4897ab59fbb31d
2hwayoung <hwa3270@gmail.com>
2022-02-06 23:39:47 +0900

    [220206] 알람시계 solved

---
+++ 제어문/2884_알람시계.py
@@ -0,0 +1,15 @@
+# https://www.acmicpc.net/problem/2884
+# 분류: if문
+
+h, m = map(int, input().split())
+
+if (m < 45):
+    m = m + 60 - 45
+    if (h == 0) :
+        h = 23
+    else :
+        h = h - 1
+else :
+    m = m - 45
+
+print(h, m)
